b4206594ededa5a0f4e63125ed9d47289a330c66
Spencer Watson <wattyrev@gmail.com>
2019-11-24 17:59:49 -0800

    Try clasprc

--- hydrateSecrets.js
+++ hydrateSecrets.js
@@ -20,3 +20,9 @@ if (fs.existsSync('./creds.json')) {
 } else if (process.env.APP_SCRIPT_CREDS) {
     fs.writeFileSync('./creds.json', process.env.APP_SCRIPT_CREDS);
 }
+
+if (fs.existsSync('./clasprc.json')) {
+    console.log('clasprc.json already exists. Leaving it alone');
+} else if (process.env.CLASPRC) {
+    fs.writeFileSync('./clasprc.json', process.env.CLASPRC);
+}
